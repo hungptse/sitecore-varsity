003b504d7703104cb9d8367e582e5fc2183c359e
Jeremy <hungpt.se@gmail.com>
2019-04-26 14:29:04 +0700

    Add Template for Course

--- Src/Feature/Course/Code/API/CourseAPIController.cs
+++ Src/Feature/Course/Code/API/CourseAPIController.cs
@@ -10,14 +10,16 @@ namespace Sitecore.Feature.Course.API
     public class CourseAPIController : ApiController
     {
         [HttpGet]
-        public IHttpActionResult GetCourseByPage([FromUri] int page)
-        {
-            if (!ModelState.IsValid)
+        public IHttpActionResult GetCourseByPage([FromUri] int page, [FromBody] Dictionary<string,string> body)
+            {
+            if (page == 0 || body == null)
             {
                 return BadRequest();
             }
+            var ID = body.FirstOrDefault(item => item.Key == "ID");
 
-            return Ok("Page " + page);
+
+            return Ok("Page " + page + " ID: " + ID);
         }
     }
 }
