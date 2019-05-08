8ed4876758c2721daf256694d2cb7c856e9f00fd
Jeremy <hungpt.se@gmail.com>
2019-05-08 10:00:03 +0700

    Replace template

--- Src/Feature/Course/Code/Templates.cs
+++ Src/Feature/Course/Code/Templates.cs
@@ -6,25 +6,25 @@ namespace Sitecore.Feature.Course
     {
         public struct CourseFolder
         {
-            public static readonly ID ID = new ID("{26BA7A68-A774-4F7D-A284-0F1CB82FC70F}");
+            public static readonly ID ID = new ID("{75918CD0-4055-4BD9-8667-666BDB0D7559}");
         }
 
         public struct CourseItem
         {
-            public static readonly ID ID = new ID("{E9A00525-80F1-4FF1-B6E0-42687582566C}");
+            public static readonly ID ID = new ID("{F2C05EA7-2295-466D-ADDB-036E77E35B24}");
 
             public struct Fields
             {
-                public static readonly ID Major = new ID("{8B1FB7C9-70B6-4C47-AE6C-785A98FFA9EA}");
-                public static readonly ID Image = new ID("{0333675D-6A9B-4A67-AC10-4CBF907AF5FC}");
-                public static readonly ID CountTime = new ID("{BEC135BF-9722-424E-AA5B-6BB077B0F98B}");
-                public static readonly ID Header = new ID("{77C5D3DA-826D-4E74-B167-3B1CF711C881}");
-                public static readonly ID Description = new ID("{C29A2694-BC68-4CC2-A7B8-88E3CE78BFFF}");
-                public static readonly ID Price = new ID("{9006BF90-ECB4-4FAE-9640-26BC460CE7E8}");
-                public static readonly ID Place = new ID("{33E709F8-F5E7-439F-91EE-5EC0DA0017C0}");
-                public static readonly ID TotalStudent = new ID("{17540395-E122-41A5-BA75-33D8149ED6C9}");
-                public static readonly ID Duration = new ID("{28744DB3-D455-4E6B-8B37-DF3B97F92C85}");
-                public static readonly ID Start = new ID("{B51B77C8-0CDD-46E4-8F85-92A6177A3EEE}");
+                public static readonly ID Major = new ID("{3762B756-B9FE-4A41-BB01-0EC8DCD03E95}");
+                public static readonly ID Image = new ID("{10E73820-443E-4C69-9513-91ACA18F0D29}");
+                public static readonly ID CountTime = new ID("{D200191F-5CE7-495D-A458-90876145E197}");
+                public static readonly ID Header = new ID("{DF3426C6-46DA-44E5-A56E-FD6D66270378}");
+                public static readonly ID Description = new ID("{D839B08B-B4FA-4023-8BBC-90AB7242B412}");
+                public static readonly ID Price = new ID("{6F7420FA-7F35-4560-B16D-D6077D31D07C}");
+                public static readonly ID Place = new ID("{8DB24D84-E54B-4068-8FD8-94AAC562E295}");
+                public static readonly ID TotalStudent = new ID("{9BD8056E-8277-4DD7-9D4C-C504830D3E7E}");
+                public static readonly ID Duration = new ID("{BF55D192-B26F-447D-904D-38A3825209A2}");
+                public static readonly ID Start = new ID("{5AA7270F-15D1-4B76-873B-C60919A44570}");
             }
         }  
         //public struct Parameters 
