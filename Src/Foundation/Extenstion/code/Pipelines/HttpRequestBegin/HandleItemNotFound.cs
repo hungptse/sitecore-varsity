ca0c64ce6a6f3e33a549d2c7b86886d06acbef9f
Jeremy <hungpt.se@gmail.com>
2019-05-08 17:57:38 +0700

    Handle 404 Page

---
+++ Src/Foundation/Extenstion/code/Pipelines/HttpRequestBegin/HandleItemNotFound.cs
@@ -0,0 +1,85 @@
+﻿using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Links;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vhs.HandleErrorProcessors.Services;
+
+namespace Vhs.HandleErrorProcessors.Pipelines.HttpRequestBegin
+{
+    public class HandleItemNotFound : HttpRequestProcessor
+    {
+        public List<string> RelativeUrlPrefixesToIgnore { get; set; }
+
+        public HandleItemNotFound()
+        {
+            RelativeUrlPrefixesToIgnore = new List<string>();
+        }
+
+        public override void Process(HttpRequestArgs args)
+        {
+            var enableHandleCustomErrors = MainUtil.GetBool(Context.Site.Properties[Constants.SiteInfo.Properties.EnableHandleCustomErrors], false);
+            if (!enableHandleCustomErrors)
+            {
+                return;
+            }
+
+            if (Context.Item != null || StartsWithPrefixToIgnore(args.Url.FilePath))
+            {
+                return;
+            }
+
+            var notFoundContent = GetNotFoundPageContent(args);
+            if (!string.IsNullOrWhiteSpace(notFoundContent))
+            {
+                args.Context.Response.TrySkipIisCustomErrors = true;
+                args.Context.Response.StatusCode = 404;
+                args.Context.Response.StatusDescription = "Page Not Found";
+                args.Context.Response.Write(notFoundContent);
+                args.Context.Response.End();
+            }
+
+            Log.Warn("The not found page: '{0}' shows no content when rendered!", args.Url);
+        }
+
+        protected bool StartsWithPrefixToIgnore(string url)
+        {
+            return !string.IsNullOrWhiteSpace(url) && RelativeUrlPrefixesToIgnore.Any(prefix => url.StartsWith(prefix, StringComparison.OrdinalIgnoreCase));
+        }
+
+        protected string GetNotFoundPageContent(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            var domain = GetDomain(args);
+
+            var notFoundItem = NotFoundItemService.GetItemBySiteProperty(Context.Site, Constants.SiteInfo.Properties.NotFoundItem);
+            var url = notFoundItem != null ? LinkManager.GetItemUrl(notFoundItem) : NotFoundItemService.GetRelativeFilePathBySiteProperty(Context.Site, Constants.SiteInfo.Properties.FallbackNotFoundItem);
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                return string.Empty;
+            }
+
+            try
+            {
+                var content = WebUtil.ExecuteWebPage(string.Concat(domain, url));
+                return content;
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{ToString()} Error - domain: {domain}, url: {url}", ex, this);
+            }
+
+            return string.Empty;
+        }
+
+        protected string GetDomain(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            return args.Context.Request.Url?.GetComponents(UriComponents.Scheme | UriComponents.Host, UriFormat.Unescaped);
+        }
+    }
+}
---
+++ Src/Foundation/Extenstion/code/Pipelines/MvcException/HandleMvcException.cs
@@ -0,0 +1,52 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Mvc.Pipelines.MvcEvents.Exception;
+using System;
+using System.Diagnostics;
+using Sitecore;
+
+namespace Vhs.HandleErrorProcessors.Pipelines.MvcException
+{
+    public class HandleMvcException : ExceptionProcessor
+    {
+        public override void Process(ExceptionArgs args)
+        {
+            try
+            {
+                var site = Context.Site;
+                if (site == null)
+                {
+                    return;
+                }
+
+                var enableHandleCustomErrors = MainUtil.GetBool(site.Properties[Constants.SiteInfo.Properties.EnableHandleCustomErrors], false);
+                if (!enableHandleCustomErrors)
+                {
+                    return;
+                }
+
+                var context = args.ExceptionContext;
+                var httpContext = context.HttpContext;
+                var exception = context.Exception;
+                if (context.ExceptionHandled || httpContext == null || exception == null)
+                {
+                    return;
+                }
+
+                var errorPageUrl = site.Properties[Constants.SiteInfo.Properties.CustomErrorFile];
+                if (string.IsNullOrWhiteSpace(errorPageUrl))
+                {
+                    return;
+                }
+
+                httpContext.Server.ClearError();
+                httpContext.Response.StatusCode = (int)System.Net.HttpStatusCode.InternalServerError;
+                httpContext.Server.Execute(errorPageUrl);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{this} {typeof(Process)} : {ex.Message}'", ex, this);
+                Log.Error($"{this} {typeof(Process)} : {ex.StackTrace}'", ex, this);
+            }
+        }
+    }
+}
