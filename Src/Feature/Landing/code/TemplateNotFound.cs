b5864f1bd9616dcc91bc2b8415011eff793315e4
Jeremy <hungpt.se@gmail.com>
2019-05-09 17:54:49 +0700

    Multil Lang

--- Src/Feature/Landing/code/TemplateNotFound.cs
+++ Src/Feature/Landing/code/TemplateNotFound.cs
@@ -15,6 +15,8 @@ namespace Sitecore.Feature.Landing
             public static readonly ID Title = new ID("{6FBBAE48-4C16-49B4-8744-DC2D697953DC}");
             public static readonly ID ErrorCode = new ID("{652606BE-9559-46BB-8783-ABA547E86C08}");
             public static readonly ID Description = new ID("{164D0838-8F04-44A2-8FC4-89FEB7B13408}");
+            public static readonly ID btnHome = new ID("{B2E00963-C8CD-45F8-A6D7-ED182758E901}");
+
         }
     }
 }
